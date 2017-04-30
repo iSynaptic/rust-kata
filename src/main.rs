df50fe9a37feea72791e7eb2a414df8ea690f51b
Jordan Terrell <jterrell@wans.net>
2017-04-30 17:44:15 -0500

    Indexing code moved into DocumentIndex impl and removed hard-code input...

--- src/main.rs
+++ src/main.rs
@@ -2,83 +2,118 @@ extern crate ansi_term;
 extern crate tantivy;
 extern crate tempdir;
 
-use std::path::Path;
-
-use ansi_term::Colour::*;
-
-use tempdir::TempDir;
-use tantivy::Index;
-use tantivy::schema::*;
-use tantivy::query::QueryParser;
-use tantivy::collector::TopCollector;
-
 fn main() {
-    print_intro();
+    rustkata::program();
+    ()
+}
 
-    if let Ok(dir) = TempDir::new("temp_dir") {
-        let path = dir.path();
-        let schema = build_schema();
+mod rustkata {
+    //use std::path::Path;
+    use ansi_term::Colour::*;
 
-        if let Ok(index) = build_index(path, schema) {
-            let searcher = index.searcher();
-            let title_field = index.schema().get_field("title").unwrap();
+    use tempdir::TempDir;
+    use tantivy::Index;
+    use tantivy::schema::*;
+    use tantivy::query::QueryParser;
+    use tantivy::collector::TopCollector;
 
-            let query_parser = QueryParser::new(index.schema(), vec![title_field]);
+    struct InputDocument {
+        name: String,
+        contents: String,
+    }
 
-            let query = query_parser.parse_query("song").unwrap();
+    struct DocumentIndex {
+        index: Index,
+        query_parser: QueryParser,
+
+        name_field: Field,
+    }
+
+    impl DocumentIndex {
+        fn search(&self, q: &str) -> Vec<String> {
+            let query = self.query_parser.parse_query(q).unwrap();
+            let searcher = self.index.searcher();
+
             let mut top_collector = TopCollector::with_limit(10);
-
             searcher.search(&*query, &mut top_collector).unwrap();
 
-            let results = top_collector.docs();
+            top_collector
+                .docs()
+                .into_iter()
+                .flat_map(|x| searcher.doc(&x))
+                .flat_map(|x| {
+                              x.get_first(self.name_field)
+                                  .map(|f| f.text().to_string())
+                          })
+                .collect::<Vec<_>>()
+        }
 
+        fn build_index<T>(input_docs: T) -> Result<DocumentIndex, ::tantivy::Error>
+            where T: Iterator<Item = InputDocument>
+        {
+            let index_dir = TempDir::new("rustkata")?;
+            let index_path = index_dir.path();
 
-            for result in results {
-                let doc = searcher.doc(&result).unwrap();
-                match doc.get_first(title_field) {
-                    Some(text) => println!("{}", text.text()),
-                    _ => (),
-                };
+            let schema = DocumentIndex::build_schema();
+
+            let index = try!(Index::create(index_path, schema.clone()));
+
+            let name_field = schema.get_field("name").unwrap();
+            let contents_field = schema.get_field("contents").unwrap();
+
+            let mut index_writer = try!(index.writer(50000000)); // 50MB buffer
+
+            for input_doc in input_docs {
+                let mut doc = Document::default();
+                doc.add_text(name_field, &input_doc.name);
+                doc.add_text(contents_field, &input_doc.contents);
+
+                index_writer.add_document(doc);
             }
 
-            println!("Done!");
-            ()
+            try!(index_writer.commit());
+            try!(index.load_searchers());
+
+            let query_parser = QueryParser::new(index.schema(), vec![name_field]);
+
+            Ok(DocumentIndex {
+                   index: index,
+                   query_parser: query_parser,
+                   name_field: name_field,
+               })
+        }
+
+        fn build_schema() -> Schema {
+            let mut schema_builder = SchemaBuilder::default();
+            schema_builder.add_text_field("name", TEXT | STORED);
+            schema_builder.add_text_field("contents", TEXT);
+
+            schema_builder.build()
         }
     }
-}
+
+    pub fn program() {
+        print_intro();
+
+        let input: Vec<InputDocument> = vec![InputDocument {
+                     name: "Never-ending Song".to_string(),
+                     contents: "This is the song that never ends. It goes on and on my friend."
+                         .to_string(),
+                 }];
 
 
-fn build_index(path: &Path, schema: Schema) -> Result<Index, tantivy::Error> {
-    let index = try!(Index::create(path, schema.clone()));
-    let title_field = schema.get_field("title").unwrap();
-    let body_field = schema.get_field("body").unwrap();
+        if let Ok(index) = DocumentIndex::build_index(input.into_iter()) {
+            let results = index.search("song");
 
-    let mut index_writer = try!(index.writer(50_000_000)); // 50MB buffer
+            for result in results {
+                println!("Result: {}", result);
+            }
+        }
+        println!("Done!");
+    }
 
-    let mut ne_song = Document::default();
-    ne_song.add_text(title_field, "Never-ending Song");
-    ne_song.add_text(body_field,
-                     "This is the song that never ends. It goes on and on my friend.");
-
-
-    index_writer.add_document(ne_song);
-
-    try!(index_writer.commit());
-    try!(index.load_searchers());
-
-    Ok(index)
-}
-
-fn build_schema() -> Schema {
-    let mut schema_builder = SchemaBuilder::default();
-    schema_builder.add_text_field("title", TEXT | STORED);
-    schema_builder.add_text_field("body", TEXT);
-
-    schema_builder.build()
-}
-
-fn print_intro() {
-    let intro = r#" (                         )                  
+    fn print_intro() {
+        let intro = r#" (                         )                  
  )\ )              )    ( /(          )       
 (()/(   (       ( /(    )\())   )  ( /(    )  
  /(_)) ))\  (   )\()) |((_)\ ( /(  )\())( /(  
@@ -88,5 +123,6 @@ fn print_intro() {
 |_|_\ \_,_|/__/ \__|  |_|\_\\__,_| \__|\__,_|
 "#;
 
-    println!("{}", Red.bold().paint(intro));
+        println!("{}", Red.bold().paint(intro));
+    }
 }
