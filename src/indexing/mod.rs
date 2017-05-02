e6dac33ea69cb6bd2bba0b5679d26983aab2ce2d
Jordan Terrell <jterrell@wans.net>
2017-05-01 21:15:03 -0500

    Restructured CLI interactive workflow into helper methods...

--- src/indexing/mod.rs
+++ src/indexing/mod.rs
@@ -5,3 +5,7 @@ mod input_document;
 pub use self::document_index::DocumentIndex;
 pub use self::document_loader::DocumentLoader;
 pub use self::input_document::InputDocument;
+
+pub fn search_by_index(term: &str, index: &DocumentIndex) -> Vec<String> {
+    index.search(&term)
+}
--- src/main.rs
+++ src/main.rs
@@ -20,26 +20,21 @@ fn main() {
     println!("Building in-memory index of sample input files...\n");
     let index = DocumentIndex::build_index(sample_docs.iter()).expect("unable to build index");
 
-
     loop {
-        print!("Enter the search term (press ENTER to exit): ");
-        io::stdout().flush().unwrap();
-
-        let mut search_term = String::new();
-        io::stdin()
-            .read_line(&mut search_term)
-            .expect("Failed to read line");
-
-        println!();
-
-        search_term = search_term.trim().to_string();
-
-        if search_term == "" {
+        let search_term = ask_for_search_term();
+        if search_term.is_none() {
             break;
         }
 
-        //let search_term = search_term;
-        let results = index.search(&search_term);
+        let search_term = search_term.unwrap();
+        let results = prompt_for_method_and_search(&search_term, &index);
+
+        if results.is_err() {
+            println!("{} - please try again.\n", results.err().unwrap());
+            continue;
+        }
+
+        let results = results.unwrap();
 
         println!("Search results:\n");
 
@@ -47,24 +42,73 @@ fn main() {
             println!("  {}", result);
         }
 
-        println!("\nWould you like to search again?");
-        print!("Type 'no' to exit. Press ENTER to search again: ");
-        io::stdout().flush().unwrap();
-
-        let mut continue_answer = String::new();
-        io::stdin()
-            .read_line(&mut continue_answer)
-            .expect("Failed to read line");
-
-        println!();
-
-        if continue_answer.trim() == "no" {
+        if !ask_should_continue() {
             break;
         }
-
     }
 
     println!("Thank you, come again!");
+}
+
+fn ask_for_search_term() -> Option<String> {
+    let mut search_term = String::new();
+    print!("Enter the search term (press ENTER to exit): ");
+    io::stdout().flush().unwrap();
+
+    io::stdin()
+        .read_line(&mut search_term)
+        .expect("Failed to read line");
+
+    println!();
+
+    search_term = search_term.trim().to_string();
+
+    if search_term == "" {
+        return None;
+    }
+
+    Some(search_term)
+}
+
+fn prompt_for_method_and_search(term: &str,
+                                index: &DocumentIndex)
+                                -> Result<Vec<String>, &'static str> {
+
+    let mut method_answer = String::new();
+    println!("Search Method: 1) String Match 2) Regular Expression 3) Indexed");
+    print!("Enter method: ");
+    io::stdout().flush().unwrap();
+
+    io::stdin()
+        .read_line(&mut method_answer)
+        .expect("Failed to read line");
+
+    println!();
+
+    match method_answer.trim() {
+        "3" => Ok(indexing::search_by_index(&term, &index)),
+        _ => Err("Unrecognized search method"),
+    }
+}
+
+fn ask_should_continue() -> bool {
+    let mut continue_answer = String::new();
+
+    println!("\nWould you like to search again?");
+    print!("Type 'no' to exit. Press ENTER to search again: ");
+    io::stdout().flush().unwrap();
+
+    io::stdin()
+        .read_line(&mut continue_answer)
+        .expect("Failed to read line");
+
+    println!();
+
+    if continue_answer.trim() == "no" {
+        return false;
+    }
+
+    true
 }
 
 fn get_sample_input_documents() -> Result<Vec<InputDocument>, std::io::Error> {
